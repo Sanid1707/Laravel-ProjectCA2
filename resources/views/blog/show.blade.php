bbc21fb45618ef9bdcfac589b673bc3c9770e193
Jason Njoku <JasNjoku@gmail.com>
2023-05-09 14:18:15 +0100

    Added code to display likes and dislikes

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -17,6 +17,9 @@
     <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
         {{ $post->description }}
     </p>
+
+    <p>Likes : {{ $post->likes }}</p>
+    <p>Dislikes : {{ $post->dislikes }}</p>
 </div>
 
 @endsection 
