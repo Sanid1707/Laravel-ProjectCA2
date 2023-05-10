bf7ebb58bdcce9807fe6076cb1fd3236cf738c96
Jason Njoku <JasNjoku@gmail.com>
2023-05-10 23:27:48 +0100

    Made the index function return the latest posts with SQL orderby

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('index');
+        return view('index')
+            ->with('posts', Post::orderBy('updated_at', 'DESC')->take(3)->get());
     }
 }
