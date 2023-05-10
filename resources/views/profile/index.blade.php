8b4e26eb4dadefba3eea9833d73f0d24c4852835
Sanidhya Pandey <92554733+Sanid1707@users.noreply.github.com>
2023-05-10 21:28:49 +0000

    Created code to display code in profile page

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -5,8 +5,9 @@
 <div class="w-4/5 m-auto text-center">
     <div class="py-15 border-b border-gray-200">
         <h1 class="text-6xl">
-            Profile
+        <span>{{ Auth::user()->name }}</span>
         </h1>
     </div>
+</div>
 
 @endsection
