d81e21e06b3de69869396e57be5eff6a5b19563c
tiao1314 <1314tiao@gmail.com>
2023-05-11 15:59:47 +0100

    edited index page content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,50 +19,49 @@
         </div>
     </div>
 
-<!-- tiao edit this :3 -->
-<!-- <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-    <div>
-        <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
-    </div>
-
-    <div class="m-auto sm:m-auto text-left w-4/5 block">
-        <h2 class="text-3xl font-extrabold text-gray-600">
-            Struggling to be a better web developer?
+    <div class="flex flex-col sm:flex-row justify-between items-center bg-gray-200 py-16 px-6">
+    <div class="w-full sm:w-1/2">
+        <h2 class="text-3xl font-extrabold text-gray-800 mb-6">
+            Explore the World through Captivating Travel Blogs
         </h2>
-
-        <p class="py-8 text-gray-500 text-s">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+        <p class="text-lg text-gray-600 mb-8">
+            Immerse yourself in inspiring travel stories and experiences shared by passionate globetrotters. Discover hidden gems, breathtaking destinations, and valuable insights that will ignite your wanderlust.
         </p>
-
-        <p class="font-extrabold text-gray-600 text-s pb-9">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos
-            molestias porro vero. Vel alias.
-        </p>
-
-        <a href="/blog" class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+        <a href="/blog" class="bg-blue-500 text-white text-lg font-semibold py-3 px-8 rounded-full hover:bg-blue-600">
             Find Out More
         </a>
     </div>
+    <div class="w-full sm:w-1/2 mt-8 sm:mt-0">
+        <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="Travel Blog Image" class="w-full">
+    </div>
 </div>
 
-<div class="text-center p-15 bg-black text-white">
-    <h2 class="text-2xl pb-5 text-l">
-        I'm an expert in...
-    </h2>
+<div class="bg-black text-white py-16 px-6">
+    <div class="max-w-3xl mx-auto">
+        <h2 class="text-2xl font-bold text-center text-white mb-8">
+            I'm an Expert in...
+        </h2>
+        <div class="grid grid-cols-2 gap-6">
+            <div class="text-center">
+                <span class="text-3xl font-semibold block mb-2">Exploring New Cultures</span>
+                <span class="text-lg text-gray-300">Discovering diverse traditions, customs, and local lifestyles.</span>
+            </div>
+            <div class="text-center">
+                <span class="text-3xl font-semibold block mb-2">Adventure Photography</span>
+                <span class="text-lg text-gray-300">Capturing breathtaking moments and stunning landscapes.</span>
+            </div>
+            <div class="text-center">
+                <span class="text-3xl font-semibold block mb-2">Solo Travel Tips</span>
+                <span class="text-lg text-gray-300">Unleashing the joys and challenges of traveling alone.</span>
+            </div>
+            <div class="text-center">
+                <span class="text-3xl font-semibold block mb-2">Food and Culinary Experiences</span>
+                <span class="text-lg text-gray-300">Indulging in authentic flavors and gastronomic adventures.</span>
+            </div>
+        </div>
+    </div>
+</div>
 
-    <span class="font-extrabold block text-4xl py-1">
-        Ux Design
-    </span>
-    <span class="font-extrabold block text-4xl py-1">
-        Project Management
-    </span>
-    <span class="font-extrabold block text-4xl py-1">
-        Digital Strategy
-    </span>
-    <span class="font-extrabold block text-4xl py-1">
-        Backend Development
-    </span>
-</div> -->
 
 <div class="text-center py-15">
     <span class="uppercase text-s text-gray-400">
