15a7edf6a6db2344c6f6d5bb5459f8a4f64d87b7
tiao1314 <97777378+tiao1314@users.noreply.github.com>
2023-05-14 18:07:05 +0000

    Added buttons to allow the user to access the private and public routes

--- resources/views/your-posts/index.blade.php
+++ resources/views/your-posts/index.blade.php
@@ -5,7 +5,7 @@
 <div class="w-4/5 m-auto text-left">
     <div class="py-15 border-b border-gray-200">
         <h1 class="text-4xl">
-        <span>Your Posts</span>
+            <span>Your Posts</span>
         </h1>
     </div>
 </div>
@@ -52,6 +52,22 @@
                     Delete
                 </button>
             </form>
+        </span>
+
+        <span class="float-right">
+            @if ($post->is_public == 0)
+                <a href="/blog/private/{{ $post->slug }}"
+                    class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
+                    Make Private
+                </a>
+                <br>
+            @else
+                <a href="/blog/public/{{ $post->slug }}"
+                    class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
+                    Make Public
+                </a>
+                <br>
+            @endif
         </span>
         @endif
     </div>
